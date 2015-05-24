b930086164067ef4c179438787da2059dcf7aeab
Zlatan Vasović <zlatanvasovic@gmail.com>
2015-05-24 16:30:32 +0200

    Update dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -46,7 +46,7 @@ module.exports = function (grunt) {
       },
       distMin: {
         options: {
-          cleancss: true
+          compress: true
         },
         src: '<%= sanitize.css %>',
         dest: '<%= sanitize.cssMin %>'
